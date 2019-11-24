6df4a667acc62976a358cb8ad7a1910b6337cc76
21300625 <21300625@handong.edu>
2019-11-24 19:46:28 +0900

    second commit

---
+++ src/back.cpp
@@ -0,0 +1,21 @@
+#include "back.h"
+#include <cstring>
+
+Back::Back()
+{
+	command = new char[strlen("back 20")+1];
+	strcpy(command, "back 20");
+}
+
+Back::Back(int _value)
+{
+	std::stringstream sstream;
+	sstream << "back" <<_value;
+	command = new char[strlen(sstream.str().c_str())+1];
+	strcpy(command,sstream.str().c_str());
+}
+
+double Back::get_delay()
+{ 
+	return 2; 
+}
